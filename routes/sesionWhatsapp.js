c95036f904822cd99a0a9bc71417465121414af4
Andresinho <rojassanchezedwinandres@gmail.com>
2026-04-29 21:47:04 -0500

    Otro mas

--- routes/sesionWhatsapp.js
+++ routes/sesionWhatsapp.js
@@ -1,6 +1,6 @@
 import express from 'express';
 export const rutasWhatsapp = express.Router();
-import {guardarSesion, verificarSesion, cerrarSesion} from '../controllers/sesionWhatsappController'
+import {guardarSesion, verificarSesion, cerrarSesion} from '../controllers/sesionWhatsappController.js'
 
 rutasWhatsapp.post('/guardar', guardarSesion);
 rutasWhatsapp.get('/verificar/:telefono', verificarSesion);
